c04167c3599587a94a54a14a8d0d7eb9b7b93470
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-06-04 20:02:05 +0200

    minor restructuring, added docstring and version

--- nanofilt/NanoFilt.py
+++ nanofilt/NanoFilt.py
@@ -4,25 +4,36 @@ Example usage:
 zcat reads.fastq.gz | NanoFilt.py -q 10 -l 500 --headcrop 50 | bwa mem -t 48 -x ont2d genome.fa - | samtools sort -O BAM -@24 -o alignment.bam -
 '''
 
+__version__= 0.1.0
 
 from Bio import SeqIO
 import argparse
 import sys
-from nanoget import aveQualFastq
+from nanomath import aveQual
+
+def main():
+    args = getArgs()
+    filterstream(sys.stdin, args)
+
 
 def getArgs():
     parser = argparse.ArgumentParser(description="Perform quality and or length filtering of Nanopore fastq data on stdin.")
+    parser.add_argument("-v", "--version", help="Print version and exit.", action="version", version='NanoFilt {}'.format(__version__))
     parser.add_argument("-q", "--quality", help="Filter on a minimum average read quality score", default=0, type=int)
     parser.add_argument("-l", "--length", help="Filter on a minimum read length", default=0, type=int)
     parser.add_argument("--headcrop", help="Trim n nucleotides from start of read", default=None, type=int)
     parser.add_argument("--tailcrop", help="Trim n nucleotides from end of read", default=None, type=int)
     return parser.parse_args()
 
-def filterstream(fq):
+
+def filterstream(fq, args):
+    '''
+    If a fastq record passes quality filter (optional) and length filter (optional), print to stdout
+    Optionally trim a number of nucleotides from beginning and end.
+    '''
     for record in SeqIO.parse(fq, "fastq"):
         if aveQualFastq(record.letter_annotations["phred_quality"]) > args.quality and len(record) > args.length:
             print(record[args.headcrop:args.tailcrop].format("fastq"), end="")
 
 if __name__ == "__main__":
-	args = getArgs()
-	filterstream(sys.stdin)
+    main()
