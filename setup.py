b18f5f38b95cbe5a2cba137cb07a4e6f6b569d8e
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-07-15 20:28:58 +0200

    add nanoget to dependencies

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     keywords='nanopore sequencing processing trimming filtering',
     packages=find_packages(),
-    install_requires=['biopython', 'nanomath>=0.12.0'],
+    install_requires=['biopython', 'nanomath>=0.12.0', 'nanoget>=0.10.6'],
     package_data={'nanofilt': []},
         package_dir={'nanofilt': 'nanofilt'},
         include_package_data=True,
