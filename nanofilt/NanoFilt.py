3b00c679dbd3bb68fc53e9420c0e027f4d133196
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-07-15 20:28:58 +0200

    make dict from df: faster

--- nanofilt/NanoFilt.py
+++ nanofilt/NanoFilt.py
@@ -9,21 +9,26 @@ from Bio import SeqIO
 import argparse
 import sys
 from nanomath import aveQual
+from nanoget import processSummary
 from nanofilt.version import __version__
 
 
 def main():
     args = getArgs()
-    filterstream(sys.stdin, args)
+    if args.summary:
+        filterusingSummary(sys.stdin, args)
+    else:
+        filterstream(sys.stdin, args)
 
 
 def getArgs():
     parser = argparse.ArgumentParser(description="Perform quality and or length filtering of Nanopore fastq data on stdin.")
     parser.add_argument("-v", "--version", help="Print version and exit.", action="version", version='NanoFilt {}'.format(__version__))
-    parser.add_argument("-q", "--quality", help="Filter on a minimum average read quality score", default=0, type=int)
     parser.add_argument("-l", "--length", help="Filter on a minimum read length", default=0, type=int)
     parser.add_argument("--headcrop", help="Trim n nucleotides from start of read", default=None, type=int)
     parser.add_argument("--tailcrop", help="Trim n nucleotides from end of read", default=None, type=int)
+    parser.add_argument("-q", "--quality", help="Filter on a minimum average read quality score", default=0, type=int)
+    parser.add_argument("-s", "--summary", help="Use summary file for quality scores")
     return parser.parse_args()
 
 
@@ -35,6 +40,21 @@ def filterstream(fq, args):
     for record in SeqIO.parse(fq, "fastq"):
         if aveQual(record.letter_annotations["phred_quality"]) > args.quality and len(record) > args.length:
             print(record[args.headcrop:args.tailcrop].format("fastq"), end="")
+
+
+def filterusingSummary(fq, args):
+    '''
+    Use the summary file from albacore for more accurate quality estimate
+    Get the dataframe from nanoget, convert to dictionary
+    '''
+    data = {entry[0]: entry[1] for entry in processSummary(args.summary)[["readIDs", "quals"]].itertuples(index=False)}
+    try:
+        for record in SeqIO.parse(fq, "fastq"):
+            if data[record.id] > args.quality and len(record) > args.length:
+                print(record[args.headcrop:args.tailcrop].format("fastq"), end="")
+    except KeyError:
+        sys.exit('\nERROR: mismatch between sequencing_summary and fastq file:\n{} was not found in the summary file.\nQuitting.'.format(record.id))
+
 
 if __name__ == "__main__":
     main()
