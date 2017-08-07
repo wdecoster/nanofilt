02b93970d12606ee4a6637a6714b11f8de032395
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-08-07 15:09:27 +0200

    negating tailcrop, changing example, layout of getArgs()

--- nanofilt/NanoFilt.py
+++ nanofilt/NanoFilt.py
@@ -1,7 +1,7 @@
 #wdecoster
 '''
 Example usage:
-zcat reads.fastq.gz | NanoFilt.py -q 10 -l 500 --headcrop 50 | bwa mem -t 48 -x ont2d genome.fa - | samtools sort -O BAM -@24 -o alignment.bam -
+gunzip -c reads.fastq.gz | NanoFilt.py -q 10 -l 500 --headcrop 50 | bwa mem -t 48 -x ont2d genome.fa - | samtools sort -O BAM -@24 -o alignment.bam -
 '''
 
 from __future__ import print_function
@@ -15,6 +15,8 @@ from nanofilt.version import __version__
 
 def main():
     args = getArgs()
+    if args.tailcrop:
+        args.tailcrop = -args.tailcrop
     if args.summary:
         filterusingSummary(sys.stdin, args)
     else:
@@ -23,12 +25,28 @@ def main():
 
 def getArgs():
     parser = argparse.ArgumentParser(description="Perform quality and or length filtering of Nanopore fastq data on stdin.")
-    parser.add_argument("-v", "--version", help="Print version and exit.", action="version", version='NanoFilt {}'.format(__version__))
-    parser.add_argument("-l", "--length", help="Filter on a minimum read length", default=0, type=int)
-    parser.add_argument("--headcrop", help="Trim n nucleotides from start of read", default=None, type=int)
-    parser.add_argument("--tailcrop", help="Trim n nucleotides from end of read", default=None, type=int)
-    parser.add_argument("-q", "--quality", help="Filter on a minimum average read quality score", default=0, type=int)
-    parser.add_argument("-s", "--summary", help="Use summary file for quality scores")
+    parser.add_argument("-v", "--version",
+                        help="Print version and exit.",
+                        action="version",
+                        version='NanoFilt {}'.format(__version__))
+    parser.add_argument("-l", "--length",
+                        help="Filter on a minimum read length",
+                        default=0,
+                        type=int)
+    parser.add_argument("--headcrop",
+                        help="Trim n nucleotides from start of read",
+                        default=None,
+                        type=int)
+    parser.add_argument("--tailcrop",
+                        help="Trim n nucleotides from end of read",
+                        default=None,
+                        type=int)
+    parser.add_argument("-q", "--quality",
+                        help="Filter on a minimum average read quality score",
+                        default=0,
+                        type=int)
+    parser.add_argument("-s", "--summary",
+                        help="Use summary file for quality scores")
     parser.add_argument("--readtype",
                         help="Which read type to extract information about from summary. Options are 1D or 2D",
                         default="1D",
