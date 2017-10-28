ac33e8e5fc1cedb6171a09dc0265b640ae10dfa1
Wouter <decosterwouter@gmail.com>
2017-10-28 18:35:58 +0200

    improved argparse

--- nanofilt/NanoFilt.py
+++ nanofilt/NanoFilt.py
@@ -23,12 +23,33 @@ gunzip -c reads.fastq.gz | \
 
 from __future__ import print_function
 from Bio import SeqIO
-from argparse import ArgumentParser, ArgumentTypeError
+from argparse import ArgumentParser, ArgumentTypeError, HelpFormatter
 import sys
 from nanomath import ave_qual
 from nanoget import process_summary
 from nanofilt.version import __version__
 import logging
+import textwrap as _textwrap
+
+
+class CustomHelpFormatter(HelpFormatter):
+    def _format_action_invocation(self, action):
+        if not action.option_strings or action.nargs == 0:
+            return super()._format_action_invocation(action)
+        default = self._get_default_metavar_for_optional(action)
+        args_string = self._format_args(action, default)
+        return ', '.join(action.option_strings) + ' ' + args_string
+
+    def _fill_text(self, text, width, indent):
+        return ''.join(indent + line for line in text.splitlines(keepends=True))
+
+    def _split_lines(self, text, width):
+        text = self._whitespace_matcher.sub(' ', text).strip()
+        return _textwrap.wrap(text, 80)
+
+
+def custom_formatter(prog):
+    return CustomHelpFormatter(prog)
 
 
 def main():
@@ -55,46 +76,64 @@ def main():
 
 
 def get_args():
+    epilog = """EXAMPLES:
+    gunzip -c reads.fastq.gz | NanoFilt -q 10 -l 500 --headcrop 50 | minimap2 genome.fa - | samtools sort -O BAM -@24 -o alignment.bam -
+    gunzip -c reads.fastq.gz | NanoFilt -q 12 --headcrop 75 | gzip > trimmed-reads.fastq.gz
+    gunzip -c reads.fastq.gz | NanoFilt -q 10 | gzip > highQuality-reads.fastq.gz
+    """
     parser = ArgumentParser(
-        description="Perform quality and/or length and/or GC filtering of Nanopore fastq data.\
-                     Reads on stdin.")
-    parser.add_argument("-v", "--version",
-                        help="Print version and exit.",
-                        action="version",
-                        version='NanoFilt {}'.format(__version__))
-    parser.add_argument("-l", "--length",
-                        help="Filter on a minimum read length",
-                        default=1,
-                        type=int)
-    parser.add_argument("--headcrop",
-                        help="Trim n nucleotides from start of read",
-                        default=None,
-                        type=int)
-    parser.add_argument("--tailcrop",
-                        help="Trim n nucleotides from end of read",
-                        default=None,
-                        type=int)
-    parser.add_argument("-q", "--quality",
-                        help="Filter on a minimum average read quality score",
-                        default=0,
-                        type=int)
-    parser.add_argument("--minGC",
-                        help="Sequences must have GC content >= to this.  Float between 0.0 and 1.0. \
+        description="Perform quality and/or length and/or GC filtering of Nanopore fastq data. Reads on stdin.",
+        epilog=epilog,
+        formatter_class=custom_formatter,
+        add_help=False)
+    general = parser.add_argument_group(
+        title='General options')
+    general.add_argument("-h", "--help",
+                         action="help",
+                         help="show the help and exit")
+    general.add_argument("-v", "--version",
+                         help="Print version and exit.",
+                         action="version",
+                         version='NanoFilt {}'.format(__version__))
+    filtering = parser.add_argument_group(
+        title='Options for filtering reads on.')
+    filtering.add_argument("-l", "--length",
+                           help="Filter on a minimum read length",
+                           default=1,
+                           type=int)
+    filtering.add_argument("-q", "--quality",
+                           help="Filter on a minimum average read quality score",
+                           default=0,
+                           type=int)
+    filtering.add_argument("--minGC",
+                           help="Sequences must have GC content >= to this.  Float between 0.0 and 1.0. \
                               Ignored if using summary file.",
-                        default=0.0,
-                        type=valid_GC)
-    parser.add_argument("--maxGC",
-                        help="Sequences must have GC content <= to this.  Float between 0.0 and 1.0. \
+                           default=0.0,
+                           type=valid_GC)
+    filtering.add_argument("--maxGC",
+                           help="Sequences must have GC content <= to this.  Float between 0.0 and 1.0. \
                               Ignored if using summary file.",
-                        default=1.0,
-                        type=valid_GC)
-    parser.add_argument("-s", "--summary",
-                        help="Use summary file for quality scores")
-    parser.add_argument("--readtype",
-                        help="Which read type to extract information about from summary. \
+                           default=1.0,
+                           type=valid_GC)
+    trimming = parser.add_argument_group(
+        title='Options for trimming reads.')
+    trimming.add_argument("--headcrop",
+                          help="Trim n nucleotides from start of read",
+                          default=None,
+                          type=int)
+    trimming.add_argument("--tailcrop",
+                          help="Trim n nucleotides from end of read",
+                          default=None,
+                          type=int)
+    inputoptions = parser.add_argument_group(
+        title='Input options.')
+    inputoptions.add_argument("-s", "--summary",
+                              help="Use summary file for quality scores")
+    inputoptions.add_argument("--readtype",
+                              help="Which read type to extract information about from summary. \
                               Options are 1D, 2D or 1D2",
-                        default="1D",
-                        choices=['1D', '2D', "1D2"])
+                              default="1D",
+                              choices=['1D', '2D', "1D2"])
     args = parser.parse_args()
     if args.minGC > args.maxGC:
         sys.exit("NanoFilt: error: argument --minGC should be smaller than --maxGC")
